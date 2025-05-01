058453eb21c99238ef9e977ee562de5d5fe6cbda
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-04-30 23:40:52 -0400

    V1

--- Ch5Ex20.cpp
+++ Ch5Ex20.cpp
@@ -23,96 +23,34 @@ void randomGenerator(int& randomNumber);
 
 int main() {
     int input;
-    int i = 0;
-    while (true) {
-        //Resets the variables between attempts
-        guess = 0;
-        guessCount = 0;
 
-        //Sets up the user to use the menu again
-        displayMenu();
-        cin >> input;
+  randomGenerator(randomNumber);
+  cout << "Guess a number between 0 and 100: " << endl;
+  cin >> guess;
 
-        //Switch case to allow inputs from 1-4
-        switch (input) {
-            //Case to check existing sessions
-        case 1:
-            cout << endl << "Session History Selected " << endl << endl;
-            if (scoreHistory.empty() == 1) {
-                cout << "No session history found!" << endl << "Sessions played will be displayed here with their scores, playing sessions should fill this with all previous games played. " << endl << endl;
-                break;
-            }
-            else {
-                while (i < scoreHistory.size()) {
-                    cout << "Your score for session " << i + 1 << " was a " << guessCount << scoreHistory[i] << endl;
-                    i++;
-                }
-                cout << endl;
-                break;
-            }
-
-            //Case to start new session
-        case 2:
-            cout << endl << "New Session Selected " << endl << endl;
-            randomGenerator(randomNumber);
-            cout << "Guess a number between 0 and 100: " << endl;
+  while (guess != randomNumber) {
+    if (guess < randomNumber && !cin.fail()) {
+        cout << "Too low, try again: " << endl;
+        guessCount++;
+        cin >> guess;
+   }
+    else if (guess > randomNumber && !cin.fail()) {
+         cout << "Too high, try again: " << endl;
+         guessCount++;
+         cin >> guess;
+   }
+   else {
+         while (cin.fail()) {
+            cin.clear(); // Clear error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            cout << "The input provided is invalid, please enter a valid input: " << endl;
             cin >> guess;
-
-            while (guess != randomNumber) {
-                if (guess < randomNumber && !cin.fail()) {
-                    cout << "Too low, try again: " << endl;
-                    guessCount++;
-                    cin >> guess;
-                }
-                else if (guess > randomNumber && !cin.fail()) {
-                    cout << "Too high, try again: " << endl;
-                    guessCount++;
-                    cin >> guess;
-                }
-                else {
-                    while (cin.fail()) {
-                        cin.clear(); // Clear error flags
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                        cout << "The input provided is invalid, please enter a valid input: " << endl;
-                        cin >> guess;
-                    }
-                }
-            }
-            cout << endl << "Correct Answer!" << endl;
-            cout << "Score: " << guessCount << endl;
-            scoreHistory.push_back(guessCount);
-            break;
-
-            //Case to clear all previous sessions from memory
-        case 3:
-            cout << endl << "Clear Previous Sessions Selected" << endl << endl;
-            if (scoreHistory.empty() == 1) {
-                cout << "No history found to be cleared!" << endl;
-            }
-            else {
-                while (i < scoreHistory.size()) {
-                    scoreHistory.pop_back();
-                    i++;
-                }
-                cout << "Succesfully removed " << scoreHistory.size() << " saved sessions!" << endl;
-            }
-            break;
-
-            //Case to exit program
-        case 4:
-            cout << endl << "Exit Program Selected" << endl << endl;
-            cout << "Closing program! " << endl;
-            exit(0);
-            break;
-
-        default:
-            if (cin.fail() || input < 1 || input > 4) {
-                cin.clear(); // Clear error flags
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                cout << "The input provided is invalid, please enter a valid input: " << endl;
-            }
-        }
-    }  
+         }
+   }
+  }
+    cout << endl << "Correct Answer!" << endl;
+    cout << "Score: " << guessCount << endl;
+    scoreHistory.push_back(guessCount);         
 }
 
 void displayMenu(){
